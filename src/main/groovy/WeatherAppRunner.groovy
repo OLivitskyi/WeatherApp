de7a12d62bb4f28d9a1d3621c8caaae5b41a39d3
oleg_livitskyi <olivitskyi@gmail.com>
2023-07-10 18:01:59 +0300

    Add functional for connection timeouts settings

--- src/main/groovy/WeatherAppRunner.groovy
+++ src/main/groovy/WeatherAppRunner.groovy
@@ -1,6 +1,35 @@
-/*
-def app = new WeatherApp()
-def city = "Tallinn"
-def weather = app.getWeatherForCity(city)
-println "Температура в ${city}: ${weather.temperature}"
-println "Рекомендація щодо одягу: ${app.recommendClothing(weather)}"*/
+package app
+
+import api.WeatherApp
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.HttpClientErrorException
+
+class WeatherAppRunner {
+
+    static void main(String[] args) {
+        WeatherApp.main(args)
+
+        def restTemplate = new RestTemplateBuilder()
+                .setConnectTimeout(5000)
+                .setReadTimeout(10000)
+                .build()
+        def url = "http://localhost:8080/weather?city=Kyiv"
+
+        try {
+            Thread.sleep(5000)
+
+            def response = restTemplate.getForEntity(url, String.class)
+
+            if (response.statusCode == HttpStatus.OK) {
+                println("Отримана відповідь: ${response.body}")
+            } else {
+                println("Сталася помилка: ${response.statusCode}")
+            }
+        } catch (HttpClientErrorException e) {
+            println("Сталася помилка HTTP: ${e.statusCode}")
+        } catch (Exception e) {
+            println("Сталася помилка: ${e.message}")
+        }
+    }
+}
